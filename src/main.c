4eb6a563202df2b7fe0c38f0165d6cc42845f9bc
mreinarrXd <jostin.angulo.gonzalez@est.una.ac.cr>
2026-03-09 18:04:17 -0600

    comando compilacion a main

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <pthread.h>
 #include "controller/controller.h"
-
+//comando compilacion: mkdir -p build && gcc -fdiagnostics-color=always -g -I src/ -I src/controller/ -I src/data/ -I src/view/ src/main.c src/controller/controller.c src/controller/guiController.c src/data/dataManager.c src/view/ui.c -o build/proyecto -pthread && ./build/proyecto
 int main() {
     pthread_t controllerThread;
     
